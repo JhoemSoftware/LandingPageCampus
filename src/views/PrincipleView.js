7033905b902e8bd444d470122f39273d692887f9
JhoemSoftware <www.jhonlive90@gmail.com>
2022-09-23 14:59:50 -0500

    Actualización detalles visuales

--- src/views/PrincipleView.js
+++ src/views/PrincipleView.js
@@ -22,7 +22,7 @@ const PrincipleView = ( {nameDev,programmingLang,company,imgJhoem} ) => {
                         <a href='#perfil' type='button' className='btn btn-primary'>perfil</a>
                     </div>
                     <div className='col-10 col-sm-3'>
-                        <a href='https://github.com/JhoemSoftware/' type='button' className='btn btn-primary'>git hub</a>
+                        <a href='https://github.com/JhoemSoftware/'  target='_blank' type='button' className='btn btn-primary'>git hub</a>
                     </div>
                 </div>
             </div>
